3abb4aa0deeef09055ebffcb2c90f26761276049
Ioana Ciornei <ciorneiioana@gmail.com>
2017-08-31 00:19:23 +0300

    packages: add docker-runc to the supported debs
    Signed-off-by: Ioana Ciornei <ciorneiioana@gmail.com>

---
+++ packages/docker-runc/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://www.opencontainers.org/
+TERMUX_PKG_DESCRIPTION="runc container cli tools"
+TERMUX_PKG_VERSION=master
+TERMUX_PKG_SHA256=146cbc43b113cb0e3b0036d6bef738f0cfe25238fd18c998473a122cc6628bc3
+TERMUX_PKG_SRCURL=https://github.com/LineageOSPlus/runc/archive/master.zip
+TERMUX_PKG_FOLDERNAME=runc-master
+
+termux_step_make(){
+	termux_setup_golang
+
+	# make folder tree look as in expected by go
+	mkdir -p go/src/github.com/opencontainers/runc
+	cp -rf $TERMUX_PKG_SRCDIR/* go/src/github.com/opencontainers/runc/
+
+	# set gopath
+	export GOPATH=$(pwd)/go
+
+	# issue the build command
+	cd go/src/github.com/opencontainers/runc/
+	make  BUILDTAGS="selinux"
+}
+
+
+termux_step_make_install() {
+	cp go/src/github.com/opencontainers/runc/runc $TERMUX_PREFIX/bin/docker-runc
+}
