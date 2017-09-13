7d324ed6e1a5c3ef9f18d221183006f38ebfad67
Vladimir Oltean <olteanv@gmail.com>
2017-09-13 12:25:54 +0300

    host scripts: remove accesses to /data from the builder environment
    * We are trying to make TERMUX_DESTDIR a subfolder of /home/builder.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -42,5 +42,3 @@ PACKAGES+=" gawk" # Needed by apr-util
 sudo DEBIAN_FRONTEND=noninteractive \
 	apt-get install -yq --no-install-recommends $PACKAGES
 
-sudo mkdir -p /data/data/com.termux/files/usr
-sudo chown -R `whoami` /data
